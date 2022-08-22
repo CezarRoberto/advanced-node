70cc9c8d163c0b52da77ecfb5cd981bec744a992
CezarRoberto <cezarrobertorodrigues50@gmail.com>
2022-08-22 09:05:20 -0300

    change SUM change strategy to use beforeEach

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,25 +1,26 @@
 import { AuthenticationError } from "@/domain/erros";
 import { FacebookAuthenticationService } from "@/data/services";
+import { LoadFacebookUserApi } from "../contracts/apis";
+import { mock, MockProxy } from "jest-mock-extended";
 
 describe("FacebookAuthenticationServices", () => {
+    let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
+    let sum: FacebookAuthenticationService;
+    beforeEach(() => {
+        loadFacebookUserApi = mock<LoadFacebookUserApi>();
+        sum = new FacebookAuthenticationService(loadFacebookUserApi);
+    });
     it("should call loadFacebookUserApi with all params", async () => {
-        const loadFacebookUserApi = {
-            loadUser: jest.fn()
-        }
-        const sum = new FacebookAuthenticationService(loadFacebookUserApi);
-
         await sum.perform({ token: "any_token" });
 
-        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({token: "any_token"});
-        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
+        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
+            token: "any_token",
+        });
+        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
     });
 
     it("should return AuthenticationError when loadFacebookUserApi returns undefined", async () => {
-        const loadFacebookUserApi = {
-            loadUser: jest.fn()
-        }
-        loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
-        const sum = new FacebookAuthenticationService(loadFacebookUserApi);
+        loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
 
         const authResult = await sum.perform({ token: "any_token" });
 
