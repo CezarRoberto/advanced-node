8363a4d5b5d7e9f2ab52e1bfe139c1e3ebb34900
CezarRoberto <cezarrobertorodrigues50@gmail.com>
2022-08-22 10:48:52 -0300

    simplificy Service Calls CreateUserFacebookAccountRepo

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,42 +1,42 @@
 import { AuthenticationError } from "@/domain/erros";
-import { CreateFacebookAccountRepository, LoadUserAccountRepository } from "../contracts/repositories";
+import {
+    CreateFacebookAccountRepository,
+    LoadUserAccountRepository,
+} from "../contracts/repositories";
 import { FacebookAuthenticationService } from "@/data/services";
 import { LoadFacebookUserApi } from "../contracts/apis";
 import { mock, MockProxy } from "jest-mock-extended";
 
 describe("FacebookAuthenticationServices", () => {
-    let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>;
-    let loadUserAccountRepo: MockProxy<LoadUserAccountRepository>;
-    let createFacebookAccountRepo: MockProxy<CreateFacebookAccountRepository>
+    let facebookApi: MockProxy<LoadFacebookUserApi>;
+    let userAccountRepo: MockProxy<
+        LoadUserAccountRepository & CreateFacebookAccountRepository
+    >;
     let sum: FacebookAuthenticationService;
     const token = "any_token";
 
     beforeEach(() => {
-        loadFacebookUserApi = mock();
-        loadUserAccountRepo = mock();
-        createFacebookAccountRepo = mock();
-        loadFacebookUserApi.loadUser.mockResolvedValue({
+        facebookApi = mock();
+        userAccountRepo = mock();
+        facebookApi.loadUser.mockResolvedValue({
             name: "any_fb_name",
             email: "any_fb_email",
             facebookId: "any_fb_id",
         });
-        sum = new FacebookAuthenticationService(
-            loadFacebookUserApi,
-            loadUserAccountRepo,
-            createFacebookAccountRepo
-        );
+        sum = new FacebookAuthenticationService(facebookApi, userAccountRepo);
     });
-    it("should call loadFacebookUserApi with all params", async () => {
+
+    it("should call loadFacebookUserApi with correct params", async () => {
         await sum.perform({ token });
 
-        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
+        expect(facebookApi.loadUser).toHaveBeenCalledWith({
             token,
         });
-        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
+        expect(facebookApi.loadUser).toHaveBeenCalledTimes(1);
     });
 
     it("should return AuthenticationError when loadFacebookUserApi returns undefined", async () => {
-        loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
+        facebookApi.loadUser.mockResolvedValueOnce(undefined);
 
         const authResult = await sum.perform({ token });
 
@@ -46,23 +46,22 @@ describe("FacebookAuthenticationServices", () => {
     it("should return LoadUserAccountRepo when loadFacebookUserApi returns data", async () => {
         await sum.perform({ token });
 
-        expect(loadUserAccountRepo.load).toHaveBeenCalledWith({
+        expect(userAccountRepo.load).toHaveBeenCalledWith({
             email: "any_fb_email",
         });
-        expect(loadUserAccountRepo.load).toHaveBeenCalledTimes(1);
+        expect(userAccountRepo.load).toHaveBeenCalledTimes(1);
     });
 
-
-    it("should call CreateUserAccount when loadFacebookUserApi returns undefined", async () => {
-        loadUserAccountRepo.load.mockResolvedValueOnce(undefined)
+    it("should call CreateUserAccount when facebookApi returns undefined", async () => {
+        userAccountRepo.load.mockResolvedValueOnce(undefined);
 
         await sum.perform({ token });
 
-        expect(createFacebookAccountRepo.createFromFacebook).toHaveBeenCalledWith({
+        expect(userAccountRepo.createFromFacebook).toHaveBeenCalledWith({
             email: "any_fb_email",
             name: "any_fb_name",
             facebookId: "any_fb_id",
         });
-        expect(createFacebookAccountRepo.createFromFacebook).toHaveBeenCalledTimes(1);
+        expect(userAccountRepo.createFromFacebook).toHaveBeenCalledTimes(1);
     });
 });
